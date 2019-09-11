17a739107df75acc893542b746a1dc70bc195c5d
user2 <user2.example.com>
2019-09-11 10:31:37 +0530

    DAO Function changed

--- Payment Solution and Liquidity Management/src/com/dao/UserAccountDetailsDAO.java
+++ Payment Solution and Liquidity Management/src/com/dao/UserAccountDetailsDAO.java
@@ -5,6 +5,5 @@ import org.json.simple.JSONObject;
 public interface UserAccountDetailsDAO {
 
 	public JSONObject updateAccountBalance(String accountNo, double balance);
-	public JSONObject poolingFunction(int userID);
-	
+   
 }
